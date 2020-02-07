012d2bf525e1f245f148cbebcbfb1c0c900666cb
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2020-02-07 18:26:44 +0700

    Fix install.sh typo and don't untar verbose

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,13 +2,13 @@
 
 echo "Installing Harmony Tx Tests"
 curl -LOs http://tools.harmony.one.s3.amazonaws.com/release/linux-x86_64/harmony-tx-tests/harmony-tx-tests && chmod u+x harmony-tx-tests
-curl -LOs http://tools.harmony.one.s3.amazonaws.com/release/linux-x86_64/harmony-tx-tests/testcases.tar.gz && tar -xzvf testcases.tar.gz && rm -rf testcases.tar.gz
+curl -LOs http://tools.harmony.one.s3.amazonaws.com/release/linux-x86_64/harmony-tx-tests/testcases.tar.gz && tar -xzf testcases.tar.gz && rm -rf testcases.tar.gz
 curl -LOs https://raw.githubusercontent.com/SebastianJ/harmony-tx-tests/master/config.yml
 mkdir -p keys/testnet
 echo "Harmony Tx Tests have now been downloaded!"
 echo ""
 echo "Make sure to either add keystore files to keys/testnet or create the file keys/testnet/private_keys.txt and add testnet private keys to it"
-echo "The tests can be run on multiple networks - testnet is just used as an example. To run the framework on other networks, e.g. localnet, use --network localnet"
+echo "The tests can be run on multiple networks - testnet is just used as an example. To run the tests on other networks, e.g. localnet, use --network localnet"
 echo ""
 echo "When you've added keyfiles or private keys, invoke the tests using ./harmony-tx-tests"
 echo ""
