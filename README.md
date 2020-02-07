4a0b20761c4d1d714be1b4e8aa5dcd3c324f81c9
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2020-02-07 18:14:18 +0700

    Update README + installer script

--- README.md
+++ README.md
@@ -3,7 +3,11 @@ Harmony-tx-tests executes a set of test cases for testing that transactions work
 
 It uses the harmony-tf testing framework under the hood.
 
-## How to implement a test case
-Test cases are defined as YAML files and placed in testcases/ - see this folder for existing test cases and how to impelement test cases.
+## Installation
 
+```
+bash <(curl -s -S -L https://raw.githubusercontent.com/SebastianJ/harmony-tx-tests/master/scripts/install.sh)
+```
 
+## Usage
+Test cases are defined as YAML files and are placed in testcases/ - see this folder for existing test cases and how to impelement test cases.
--- scripts/install.sh
+++ scripts/install.sh
@@ -5,8 +5,11 @@ curl -LOs http://tools.harmony.one.s3.amazonaws.com/release/linux-x86_64/harmony
 curl -LOs http://tools.harmony.one.s3.amazonaws.com/release/linux-x86_64/harmony-tx-tests/testcases.tar.gz && tar -xzvf testcases.tar.gz && rm -rf testcases.tar.gz
 curl -LOs https://raw.githubusercontent.com/SebastianJ/harmony-tx-tests/master/config.yml
 mkdir -p keys/testnet
-echo "Harmony Tx Tests have now been downloaded"
+echo "Harmony Tx Tests have now been downloaded!"
+echo ""
 echo "Make sure to either add keystore files to keys/testnet or create the file keys/testnet/private_keys.txt and add testnet private keys to it"
-echo "The tests can be run on multiple network - testnet is just used as an example. To run the framework on other networks, e.g. localnet, use --network localnet"
+echo "The tests can be run on multiple networks - testnet is just used as an example. To run the framework on other networks, e.g. localnet, use --network localnet"
+echo ""
 echo "When you've added keyfiles or private keys, invoke the tests using ./harmony-tx-tests"
+echo ""
 echo "To see all available configuration options, run ./harmony-tx-tests --help or check out the configuration specified in config.yml"
