2de2df703a699274b495226eeab6314fcf05304a
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2020-02-07 18:49:44 +0700

    Update installer script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Installing Harmony Tx Tests"
-curl -LOs http://tools.harmony.one.s3.amazonaws.com/release/linux-x86_64/harmony-tx-tests/harmony-tx-tests && chmod u+x harmony-tx-tests
-curl -LOs http://tools.harmony.one.s3.amazonaws.com/release/linux-x86_64/harmony-tx-tests/testcases.tar.gz && tar -xzf testcases.tar.gz && rm -rf testcases.tar.gz
+rm -rf harmony-tx-tests && curl -LOs http://tools.harmony.one.s3.amazonaws.com/release/linux-x86_64/harmony-tx-tests/harmony-tx-tests && chmod u+x harmony-tx-tests
+rm -rf testcases && curl -LOs http://tools.harmony.one.s3.amazonaws.com/release/linux-x86_64/harmony-tx-tests/testcases.tar.gz && tar -xzf testcases.tar.gz && rm -rf testcases.tar.gz
 curl -LOs https://raw.githubusercontent.com/SebastianJ/harmony-tx-tests/master/config.yml
 mkdir -p keys/testnet
 echo "Harmony Tx Tests have now been downloaded!"
