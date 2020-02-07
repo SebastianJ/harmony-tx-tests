8c21904daa161a6c00ae48ab36e38c0370be8464
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2020-02-07 21:23:05 +0700

    Only download testcases if they don't exist - to prevent people from losing their testcase changes

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 
 echo "Installing Harmony Tx Tests"
 rm -rf harmony-tx-tests && curl -LOs http://tools.harmony.one.s3.amazonaws.com/release/linux-x86_64/harmony-tx-tests/harmony-tx-tests && chmod u+x harmony-tx-tests
-rm -rf testcases && curl -LOs http://tools.harmony.one.s3.amazonaws.com/release/linux-x86_64/harmony-tx-tests/testcases.tar.gz && tar -xzf testcases.tar.gz && rm -rf testcases.tar.gz
+[ ! -d testcases ] && curl -LOs http://tools.harmony.one.s3.amazonaws.com/release/linux-x86_64/harmony-tx-tests/testcases.tar.gz && tar -xzf testcases.tar.gz && rm -rf testcases.tar.gz
 [ ! -f config.yml ] && curl -LOs https://raw.githubusercontent.com/SebastianJ/harmony-tx-tests/master/config.yml
 mkdir -p keys/testnet
 echo "Harmony Tx Tests have now been downloaded!"
